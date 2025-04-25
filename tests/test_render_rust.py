68d65eef9bd2e8ecf7f8ac89cb5194ca2e8ef366
Ryan Bartling <rbartling@axon.com>
2025-04-25 10:10:22 -0700

    test: Match unit tests expectations to results

--- tests/test_render_rust.py
+++ tests/test_render_rust.py
@@ -28,7 +28,6 @@ mod my_file {
 #![allow(dead_code)]
 
 use modular_bitfield::prelude::*;
-use zerocopy::*;
 
 #[cfg(feature = "std")]
 use std::fmt::{Display, Formatter};
@@ -82,7 +81,6 @@ mod my_file {
 #![allow(dead_code)]
 
 use modular_bitfield::prelude::*;
-use zerocopy::*;
 
 #[cfg(feature = "std")]
 use std::fmt::{Display, Formatter};
@@ -90,12 +88,11 @@ use std::fmt::{Display, Formatter};
 pub type temperature_units_slice = [u8;  1];
 // Temperature units
 // The temperature units
-#[repr(u8)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(
-    Default, Debug, Clone, ByteEq, ByteHash, Immutable, KnownLayout, IntoBytes, TryFromBytes, BitfieldSpecifier,
-)]
+    Default, Debug, Clone, PartialEq, Copy,
+)]#[derive(BitfieldSpecifier)]
 #[bits = 1]
+
 pub enum temperature_units {
 #[default]
 /// Degrees Celsius
@@ -103,11 +100,41 @@ c = 0x0,
 /// Degrees Fahrenheit
 f = 0x1,
 }
-const TEMPERATURE_UNITS_SIZE_ASSERT: [u8; 1] = [0; std::mem::size_of::<temperature_units>()];
 
 impl From<temperature_units> for temperature_units_slice {
 fn from(value: temperature_units) -> Self {
-transmute!(value)
+let v = value as u8;
+v.to_le_bytes()
+}
+}
+
+impl TryFrom<&[u8]> for temperature_units {
+type Error = ();
+fn try_from(value: &[u8]) -> Result<Self, ()> {
+assert!(value.len() >= size_of::<temperature_units_slice>());
+let v: temperature_units_slice = value[..size_of::<temperature_units_slice>()]
+.try_into()
+.map_err(|_| ())?;
+Self::try_from(v)
+}
+}
+
+impl TryFrom<temperature_units_slice> for temperature_units {
+type Error = ();
+fn try_from(value: temperature_units_slice) -> Result<Self, ()> {
+let v = u8::from_le_bytes(value);
+v.try_into()
+}
+}
+
+impl TryFrom<u8> for temperature_units {
+type Error = ();
+fn try_from(value: u8) -> Result<Self, ()> {
+match value {
+0 => Ok(temperature_units::c),
+1 => Ok(temperature_units::f),
+_ => Err(()),
+}
 }
 }
 
@@ -161,7 +188,6 @@ mod my_file {
 #![allow(dead_code)]
 
 use modular_bitfield::prelude::*;
-use zerocopy::*;
 
 #[cfg(feature = "std")]
 use std::fmt::{Display, Formatter};
@@ -169,18 +195,50 @@ use std::fmt::{Display, Formatter};
 pub type cmd_temperature_set_slice = [u8;  3];
 // Request temperature change
 // Request a change in temperature
-#[repr(packed)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(
-    Default, Debug, Clone, ByteEq, ByteHash, Immutable, KnownLayout, IntoBytes, TryFromBytes,
+    Debug, Clone, PartialEq,
 )]
 pub struct cmd_temperature_set{
 /// Desired temperature
-temperature: i16,
+pub temperature: i16,
 /// Selected temperature unit
-units: u8,
+pub units: u8,
 }
-const CMD_TEMPERATURE_SET_SIZE_ASSERT: [u8; 3] = [0; std::mem::size_of::<cmd_temperature_set>()];
+
+impl From<cmd_temperature_set> for cmd_temperature_set_slice {
+#[allow(unused_variables)]
+fn from(value: cmd_temperature_set) -> Self {
+#[allow(unused_mut)]
+let mut buf: cmd_temperature_set_slice = [0; 3];
+
+buf[0..2].copy_from_slice(&value.temperature.to_le_bytes());
+buf[2..3].copy_from_slice(&value.units.to_le_bytes());
+
+buf
+}
+}
+
+impl TryFrom<cmd_temperature_set_slice> for cmd_temperature_set {
+type Error = ();
+fn try_from(input: cmd_temperature_set_slice) -> Result<Self, ()> {
+    let input: &[u8] = &input;
+    input.try_into()
+}
+}
+
+impl TryFrom<&[u8]> for cmd_temperature_set {
+type Error = ();
+fn try_from(input: &[u8]) -> Result<Self, ()> {
+assert!(input.len() >= size_of::<cmd_temperature_set_slice>());
+
+Ok(Self{
+temperature: i16::from_le_bytes(input[0..2].try_into().unwrap()),
+units: u8::from_le_bytes(input[2..3].try_into().unwrap()),
+})
+
+}
+}
 
 }
 """
@@ -218,7 +276,6 @@ mod my_file {
 #![allow(dead_code)]
 
 use modular_bitfield::prelude::*;
-use zerocopy::*;
 
 #[cfg(feature = "std")]
 use std::fmt::{Display, Formatter};
@@ -316,64 +373,125 @@ mod my_file {
 #![allow(dead_code)]
 
 use modular_bitfield::prelude::*;
-use zerocopy::*;
 
 #[cfg(feature = "std")]
 use std::fmt::{Display, Formatter};
 
-pub type commands_slice = [u8;  6];
+pub type commands_slice = [u8;  5];
 #[repr(u16)]
-#[derive(Debug, Clone, ByteEq, ByteHash, Immutable, KnownLayout, IntoBytes, TryFromBytes,)]
+#[derive(Debug, Clone, PartialEq, )]
 pub enum commands {
-reset(cmd_reset, [u8;4]) = 1,
-temperature_set(cmd_temperature_set, [u8;1]) = 2,
+reset(cmd_reset) = 1,
+temperature_set(cmd_temperature_set) = 2,
 }
-const COMMANDS_SIZE_ASSERT: [u8; 6] = [0; std::mem::size_of::<commands>()];
+impl commands {
+pub fn size(&self) -> usize {
+match self{
+commands::reset(_) => 2,
+commands::temperature_set(_) => 5,
 
+}
+}
+}
 impl From<commands> for commands_slice {
-    fn from(value: commands) -> Self {
-        transmute!(value)
-    }
+fn from(value: commands) -> Self {
+#[allow(unused_mut)]
+let mut buf = [0_u8; 5];
+match value {
+commands::reset(inner) => {
+buf[0..2].copy_from_slice(&1_u16.to_le_bytes());
+let inner_buf: cmd_reset_slice = inner.into();
+buf[2..2].copy_from_slice(&inner_buf);
 }
-
+commands::temperature_set(inner) => {
+buf[0..2].copy_from_slice(&2_u16.to_le_bytes());
+let inner_buf: cmd_temperature_set_slice = inner.into();
+buf[2..5].copy_from_slice(&inner_buf);
+}
+}
+buf
+}
+}
 impl TryFrom<&[u8]> for commands {
-    type Error = ();
+type Error = ();
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        let r = commands::try_ref_from_bytes(value).map_err(|_| ())?;
-        Ok(r.to_owned())
-    }
+fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+let repr_int = u16::from_le_bytes(value[0..2].try_into().unwrap());
+match repr_int {
+1 => {
+let inner_buf: &[u8] = &value[2..];
+let inner = inner_buf.try_into()?;
+Ok(commands::reset(inner))
+}
+2 => {
+let inner_buf: &[u8] = &value[2..];
+let inner = inner_buf.try_into()?;
+Ok(commands::temperature_set(inner))
+}
+_ => Err(()),
+}
+}
 }
 
 impl TryFrom<commands_slice> for commands {
-    type Error = ValidityError<commands_slice, commands>;
+type Error = ();
 
-    fn try_from(value: commands_slice) -> Result<Self, Self::Error> {
-        try_transmute!(value)
-    }
+fn try_from(value: commands_slice) -> Result<Self, Self::Error> {
+let r: &[u8] = &value;
+r.try_into()
+}
 }
 pub type cmd_reset_slice = [u8;  0];
 // Reset Request
 // Request a software reset
-#[repr(packed)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(
-    Default, Debug, Clone, ByteEq, ByteHash, Immutable, KnownLayout, IntoBytes, TryFromBytes,
+    Debug, Clone, PartialEq,
 )]
 pub struct cmd_reset{
 // Structure is intentionally empty (zero sized)
 }
-const CMD_RESET_SIZE_ASSERT: [u8; 0] = [0; std::mem::size_of::<cmd_reset>()];
+
+impl From<cmd_reset> for cmd_reset_slice {
+#[allow(unused_variables)]
+fn from(value: cmd_reset) -> Self {
+#[allow(unused_mut)]
+let mut buf: cmd_reset_slice = [0; 0];
+
+
+
+buf
+}
+}
+
+impl TryFrom<cmd_reset_slice> for cmd_reset {
+type Error = ();
+fn try_from(input: cmd_reset_slice) -> Result<Self, ()> {
+    let input: &[u8] = &input;
+    input.try_into()
+}
+}
+
+impl TryFrom<&[u8]> for cmd_reset {
+type Error = ();
+fn try_from(input: &[u8]) -> Result<Self, ()> {
+assert!(input.len() >= size_of::<cmd_reset_slice>());
+
+Ok(Self{
+
+})
+
+}
+}
 
 pub type temperature_units_slice = [u8;  1];
 // Temperature units
 // The temperature units
-#[repr(u8)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(
-    Default, Debug, Clone, ByteEq, ByteHash, Immutable, KnownLayout, IntoBytes, TryFromBytes, BitfieldSpecifier,
-)]
+    Default, Debug, Clone, PartialEq, Copy,
+)]#[derive(BitfieldSpecifier)]
 #[bits = 1]
+
 pub enum temperature_units {
 #[default]
 /// Degrees Celsius
@@ -381,33 +499,96 @@ c = 0x0,
 /// Degrees Fahrenheit
 f = 0x1,
 }
-const TEMPERATURE_UNITS_SIZE_ASSERT: [u8; 1] = [0; std::mem::size_of::<temperature_units>()];
 
 impl From<temperature_units> for temperature_units_slice {
 fn from(value: temperature_units) -> Self {
-transmute!(value)
+let v = value as u8;
+v.to_le_bytes()
+}
+}
+
+impl TryFrom<&[u8]> for temperature_units {
+type Error = ();
+fn try_from(value: &[u8]) -> Result<Self, ()> {
+assert!(value.len() >= size_of::<temperature_units_slice>());
+let v: temperature_units_slice = value[..size_of::<temperature_units_slice>()]
+.try_into()
+.map_err(|_| ())?;
+Self::try_from(v)
+}
+}
+
+impl TryFrom<temperature_units_slice> for temperature_units {
+type Error = ();
+fn try_from(value: temperature_units_slice) -> Result<Self, ()> {
+let v = u8::from_le_bytes(value);
+v.try_into()
+}
+}
+
+impl TryFrom<u8> for temperature_units {
+type Error = ();
+fn try_from(value: u8) -> Result<Self, ()> {
+match value {
+0 => Ok(temperature_units::c),
+1 => Ok(temperature_units::f),
+_ => Err(()),
+}
 }
 }
 
 pub type cmd_temperature_set_slice = [u8;  3];
 // Request temperature change
 // Request a change in temperature
-#[repr(packed)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(
-    Default, Debug, Clone, ByteEq, ByteHash, Immutable, KnownLayout, IntoBytes, TryFromBytes,
+    Debug, Clone, PartialEq,
 )]
 pub struct cmd_temperature_set{
 /// Desired temperature
-temperature: i16,
+pub temperature: i16,
 /// Selected temperature unit
-units: [u8; 1],
+pub units: temperature_units,
 }
-const CMD_TEMPERATURE_SET_SIZE_ASSERT: [u8; 3] = [0; std::mem::size_of::<cmd_temperature_set>()];
+
+impl From<cmd_temperature_set> for cmd_temperature_set_slice {
+#[allow(unused_variables)]
+fn from(value: cmd_temperature_set) -> Self {
+#[allow(unused_mut)]
+let mut buf: cmd_temperature_set_slice = [0; 3];
+
+buf[0..2].copy_from_slice(&value.temperature.to_le_bytes());
+let temp: [u8; 1] = value.units.into();
+buf[2..3].copy_from_slice(&temp);
+
+
+buf
+}
+}
+
+impl TryFrom<cmd_temperature_set_slice> for cmd_temperature_set {
+type Error = ();
+fn try_from(input: cmd_temperature_set_slice) -> Result<Self, ()> {
+    let input: &[u8] = &input;
+    input.try_into()
+}
+}
+
+impl TryFrom<&[u8]> for cmd_temperature_set {
+type Error = ();
+fn try_from(input: &[u8]) -> Result<Self, ()> {
+assert!(input.len() >= size_of::<cmd_temperature_set_slice>());
+
+Ok(Self{
+temperature: i16::from_le_bytes(input[0..2].try_into().unwrap()),
+units: input[2..3].try_into()?,
+})
+
+}
+}
 
 }
 """
-    print(result)
     assert expected == result
 
 
@@ -458,7 +639,6 @@ mod my_file {
 #![allow(dead_code)]
 
 use modular_bitfield::prelude::*;
-use zerocopy::*;
 
 #[cfg(feature = "std")]
 use std::fmt::{Display, Formatter};
@@ -466,11 +646,10 @@ use std::fmt::{Display, Formatter};
 pub type cmd_temperature_set_slice = [u8;  2];
 // Request temperature change
 // Request a change in temperature
-#[repr(u16)]
 #[bitfield]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(
-    Default, Debug, Clone, ByteEq, ByteHash, Immutable, KnownLayout, IntoBytes, TryFromBytes,
+    Debug, Clone, PartialEq, Copy,
 )]
 pub struct cmd_temperature_set{
 /// Desired temperature
@@ -478,13 +657,33 @@ pub temperature: int,
 #[skip]
 reserved_8: B1,
 /// Selected temperature unit
-pub units: uint,
+pub units: B2,
 #[skip]
 reserved_11: B5,
 }
-const CMD_TEMPERATURE_SET_SIZE_ASSERT: [u8; 2] = [0; std::mem::size_of::<cmd_temperature_set>()];
+
+impl From<cmd_temperature_set> for cmd_temperature_set_slice {
+fn from(input: cmd_temperature_set) -> Self {
+input.into_bytes()
+}
+}
+
+impl TryFrom<cmd_temperature_set_slice> for cmd_temperature_set {
+type Error = ();
+fn try_from(input: cmd_temperature_set_slice) -> Result<Self, ()> {
+Ok(Self::from_bytes(input))
+}
+}
+
+impl TryFrom<&[u8]> for cmd_temperature_set {
+type Error = ();
+fn try_from(input: &[u8]) -> Result<Self, ()> {
+assert!(input.len() >= size_of::<cmd_temperature_set_slice>());
+let a: cmd_temperature_set_slice = input.try_into().unwrap();
+Ok(Self::from_bytes(a))
+}
+}
 
 }
 """
-    print(result)
     assert expected == result
