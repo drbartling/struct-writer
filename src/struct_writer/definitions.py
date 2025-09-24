28a85f423aec240d5acc85e8b67349a90d86bf99
Ryan Bartling <ryan.bartling@gmail.com>
2025-09-23 23:03:51 -0700

    feat: Parse example file into defined types

--- src/struct_writer/definitions.py
+++ src/struct_writer/definitions.py
@@ -1,7 +1,6 @@
+import math
 from dataclasses import dataclass
 from typing import Any, Self, override
-
-from devtools import debug
 
 
 class ParseFailed(Exception):
@@ -12,10 +11,35 @@ class ParseFailed(Exception):
 
 class SizeMismatch(ParseFailed):
     def __init__(
-        self, name: str, expected_size: int, measured_size: int
+        self, name: str, expected_size: int, measured_size: float
     ) -> None:
-        self.message: str = f"Expected {name} to be size {expected_size}, but the sum of it's members is {measured_size}"
+        self.message: str = f"Expected {name} to be size {expected_size}, but measured a size of {measured_size} bytes"
         super().__init__(self.message)
+
+
+class InvalidType(ParseFailed):
+    """ """
+
+    def __init__(self, name: str, def_type: str) -> None:
+        self.message: str = f"Invalid type (`{def_type}`) given for {name}"
+        super().__init__(self.message)
+
+
+class RequiredFieldMissing(ParseFailed):
+    def __init__(self, def_type: str, name: str, field: str) -> None:
+        self.def_type: str = def_type
+        self.name: str = name
+        self.field: str = field
+        self.message: str = (
+            f"`{name}` definition for `{def_type}` missing `{field}`"
+        )
+        super().__init__(self.message)
+
+    @classmethod
+    def from_key_error(
+        cls, def_type: str, name: str, key_error: KeyError
+    ) -> Self:
+        return cls(def_type, name, key_error.args[0])
 
 
 class DefinedType:
@@ -30,10 +54,6 @@ class StructureMember:
     name: str
     size: int
     type: str
-
-    @override
-    def __repr__(self) -> str:
-        return f"{self.__class__!s}({self.__dict__!r})"
 
     @classmethod
     def from_dict(cls, definition: dict[str, Any]) -> Self:
@@ -53,10 +73,6 @@ class Structure(DefinedType):
     name: str
     size: int
 
-    @override
-    def __repr__(self) -> str:
-        return f"{self.__class__!s}({self.__dict__!r})"
-
     @classmethod
     def from_dict(cls, definition: dict[str, dict[str, Any]]) -> Self:
         (name, inner) = next(iter(definition.items()))
@@ -64,8 +80,6 @@ class Structure(DefinedType):
 
     @classmethod
     def from_named_dict(cls, name: str, definition: dict[str, Any]) -> Self:
-        debug(name)
-        debug(definition)
         members = [
             StructureMember.from_dict(m) for m in definition.get("members", [])
         ]
@@ -83,6 +97,229 @@ class Structure(DefinedType):
 
 
 @dataclass
+class EnumValue:
+    label: str
+    value: int
+    display_name: str
+    description: str
+
+    @classmethod
+    def from_dict(cls, definition: dict[str, Any]) -> Self:
+        try:
+            return cls(
+                label=definition["label"],
+                value=definition["value"],
+                display_name=definition["display_name"],
+                description=definition["description"],
+            )
+        except KeyError as e:
+            raise RequiredFieldMissing.from_key_error(
+                definition.get("label", "missing_label"),
+                cls.__name__,
+                e,
+            ) from e
+
+    @classmethod
+    def from_partial_dict(
+        cls, definition: dict[str, Any], default_val: int
+    ) -> Self:
+        definition["value"] = definition.get("value", default_val)
+        return cls.from_dict(definition)
+
+
+@dataclass
+class Enumeration(DefinedType):
+    name: str
+    values: list[EnumValue]
+    description: str
+    display_name: str
+    size: int
+
+    @classmethod
+    def from_named_dict(cls, name: str, definition: dict[str, Any]) -> Self:
+        values: list[EnumValue] = []
+        next_int = 0
+        try:
+            for v in definition.get("values", []):
+                ev = EnumValue.from_partial_dict(v, next_int)
+                next_int = ev.value + 1
+                values.append(ev)
+        except RequiredFieldMissing as e:
+            def_type = f"{name}::{e.def_type}"
+            raise RequiredFieldMissing(def_type, e.name, e.field) from e
+        is_signed = min(v.value for v in values) < 0
+        max_val = max(abs(v.value) for v in values)
+        bits = math.ceil(math.log2(max_val))
+        bits = bits + 2 if is_signed else bits + 1
+        measured_size = math.ceil(bits / 8.0)
+        enum_size = definition["size"]
+        if measured_size != enum_size:
+            raise SizeMismatch(name, enum_size, measured_size)
+        return cls(
+            description=definition["description"],
+            display_name=definition["display_name"],
+            values=values,
+            name=name,
+            size=enum_size,
+        )
+
+
+@dataclass
+class BitFieldMember:
+    name: str
+    start: int
+    end: int
+    bits: int
+    type: str
+    description: str
+
+    @classmethod
+    def from_dict(cls, definition: dict[str, Any]) -> Self:
+        try:
+            return cls(
+                name=definition["name"],
+                start=definition["start"],
+                end=definition["end"],
+                bits=definition["bits"],
+                type=definition["type"],
+                description=definition["description"],
+            )
+        except KeyError as e:
+            raise RequiredFieldMissing.from_key_error(
+                definition.get("label", "missing_label"),
+                cls.__name__,
+                e,
+            ) from e
+
+    @classmethod
+    def from_partial_dict(
+        cls,
+        definition: dict[str, Any],
+        default_start: int,
+        default_bits: int = 1,
+    ) -> Self:
+        definition["start"] = definition.get("start", default_start)
+        if end := definition.get("end"):
+            definition["bits"] = definition.get(
+                "bits", end - definition["start"]
+            )
+        else:
+            definition["bits"] = definition.get("bits", default_bits)
+            definition["end"] = definition["start"] + definition["bits"]
+        return cls.from_dict(definition)
+
+    @classmethod
+    def reserved(cls, start: int, end: int) -> Self:
+        bits = end - start
+        return cls(
+            name=f"reserved_{start}",
+            start=start,
+            end=end,
+            bits=bits,
+            type="reserved",
+            description="Reserved",
+        )
+
+
+@dataclass
+class BitField(DefinedType):
+    description: str
+    display_name: str
+    members: list[BitFieldMember]
+    name: str
+    size: int
+
+    @classmethod
+    def from_dict(cls, definition: dict[str, dict[str, Any]]) -> Self:
+        (name, inner) = next(iter(definition.items()))
+        return cls.from_named_dict(name, inner)
+
+    @classmethod
+    def from_named_dict(cls, name: str, definition: dict[str, Any]) -> Self:
+        members: list[BitFieldMember] = []
+        start_bit = 0
+        for m in definition.get("members", []):
+            if start_bit < m.get("start", start_bit):
+                members.append(BitFieldMember.reserved(start_bit, m["start"]))
+            bfm = BitFieldMember.from_partial_dict(m, start_bit)
+            start_bit = bfm.end
+            members.append(bfm)
+        structure_size: int = definition["size"]
+        expected_bits: int = structure_size * 8
+        measured_bits = sum(m.bits for m in members)
+        if measured_bits > expected_bits:
+            raise SizeMismatch(name, structure_size, measured_bits / 8.0)
+        reserved_bits = expected_bits - measured_bits
+        if measured_bits < expected_bits:
+            members.append(
+                BitFieldMember.reserved(start_bit, start_bit + reserved_bits)
+            )
+
+        return cls(
+            description=definition["description"],
+            display_name=definition["display_name"],
+            members=members,
+            name=name,
+            size=structure_size,
+        )
+
+
+@dataclass
+class GroupMember:
+    # The name used within the group
+    # e.g. the variable name for the member in a C union
+    name: str
+    # The name of the type this member is
+    type: str
+    # The TAG value used to descriminate the type
+    value: int
+
+    @classmethod
+    def from_named_dict(cls, name: str, dictionary: dict[str, Any]) -> Self:
+        return cls(
+            name=dictionary["name"],
+            type=name,
+            value=dictionary["value"],
+        )
+
+
+@dataclass
+class Group(DefinedType):
+    """
+    A Tagged Union of a group of structures
+
+    https://en.wikipedia.org/wiki/Tagged_union
+
+    """
+
+    name: str
+    description: str
+    size: int
+    members: list[GroupMember]
+
+    @classmethod
+    def from_definitions(
+        cls, group_name: str, definitions: dict[str, Any]
+    ) -> Self:
+        members = [
+            k
+            for (k, v) in definitions.items()
+            if group_name in v.get("groups", {})
+        ]
+        members = {m: definitions[m]["groups"][group_name] for m in members}
+        members = [
+            GroupMember.from_named_dict(k, v) for (k, v) in members.items()
+        ]
+        group_definition = definitions[group_name]
+        return cls(
+            name=group_name,
+            description=group_definition["description"],
+            size=group_definition["size"],
+            members=members,
+        )
+
+
+@dataclass
 class TypeDefinitions:
     definitions: dict[str, DefinedType]
 
@@ -93,7 +330,19 @@ class TypeDefinitions:
     @classmethod
     def from_dict(cls, definitions: dict[str, Any]) -> Self:
         result: dict[str, DefinedType] = {}
+        _file_info = definitions.pop("file", None)
         for k, v in definitions.items():
-            result[k] = Structure.from_named_dict(k, v)
+            definition_type: str = v.get("type")
+            match definition_type:
+                case "bit_field":
+                    result[k] = BitField.from_named_dict(k, v)
+                case "enum":
+                    result[k] = Enumeration.from_named_dict(k, v)
+                case "group":
+                    result[k] = Group.from_definitions(k, definitions)
+                case "structure":
+                    result[k] = Structure.from_named_dict(k, v)
+                case _:
+                    raise InvalidType(k, definition_type)
 
         return cls(result)
--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -1,14 +1,22 @@
+from pathlib import Path
 from typing import Any
 
 import pytest
 from devtools import debug
 
 from struct_writer.definitions import (
+    BitField,
+    BitFieldMember,
+    Enumeration,
+    EnumValue,
+    Group,
+    GroupMember,
     ParseFailed,
     Structure,
     StructureMember,
     TypeDefinitions,
 )
+from struct_writer.generate_structured_code import load_markup_file
 
 structure_params = [
     (
@@ -104,6 +112,7 @@ type_definitions_params = [
             "empty_struct": {
                 "description": "An empty structure",
                 "display_name": "Empty Structure",
+                "type": "structure",
                 "size": 0,
             }
         },
@@ -125,6 +134,7 @@ type_definitions_params = [
             "simple_struct": {
                 "description": "A simple structure",
                 "display_name": "Simple Structure",
+                "type": "structure",
                 "size": 1,
                 "members": [
                     {
@@ -155,6 +165,190 @@ type_definitions_params = [
             }
         ),
     ),
+    (
+        "Enum",
+        {
+            "an_enum": {
+                "description": "an example enum",
+                "display_name": "An Enum",
+                "type": "enum",
+                "size": 1,
+                "values": [
+                    {
+                        "label": "a",
+                        "value": 0,
+                        "display_name": "A",
+                        "description": "The letter A",
+                    },
+                    {
+                        "label": "b",
+                        "value": 1,
+                        "display_name": "B",
+                        "description": "The letter B",
+                    },
+                ],
+            }
+        },
+        TypeDefinitions(
+            {
+                "an_enum": Enumeration(
+                    name="an_enum",
+                    description="an example enum",
+                    display_name="An Enum",
+                    size=1,
+                    values=[
+                        EnumValue(
+                            label="a",
+                            value=0,
+                            display_name="A",
+                            description="The letter A",
+                        ),
+                        EnumValue(
+                            label="b",
+                            value=1,
+                            display_name="B",
+                            description="The letter B",
+                        ),
+                    ],
+                )
+            }
+        ),
+    ),
+    (
+        "Empty Bit Field",
+        {
+            "a_bit_field": {
+                "description": "An example bit field",
+                "display_name": "A Bit Field",
+                "type": "bit_field",
+                "size": 0,
+            },
+        },
+        TypeDefinitions(
+            {
+                "a_bit_field": BitField(
+                    description="An example bit field",
+                    display_name="A Bit Field",
+                    members=[],
+                    name="a_bit_field",
+                    size=0,
+                )
+            }
+        ),
+    ),
+    (
+        "Simple Bit Field",
+        {
+            "a_bit_field": {
+                "description": "An example bit field",
+                "display_name": "A Bit Field",
+                "type": "bit_field",
+                "size": 1,
+                "members": [
+                    {
+                        "name": "a_number",
+                        "start": 0,
+                        "end": 4,
+                        "bits": 4,
+                        "type": "uint",
+                        "description": "A 4 bit number",
+                    },
+                    {
+                        "name": "reserved_4",
+                        "start": 4,
+                        "end": 8,
+                        "bits": 4,
+                        "type": "reserved",
+                        "description": "Unused bits",
+                    },
+                ],
+            },
+        },
+        TypeDefinitions(
+            {
+                "a_bit_field": BitField(
+                    description="An example bit field",
+                    display_name="A Bit Field",
+                    members=[
+                        BitFieldMember(
+                            name="a_number",
+                            start=0,
+                            end=4,
+                            bits=4,
+                            type="uint",
+                            description="A 4 bit number",
+                        ),
+                        BitFieldMember(
+                            name="reserved_4",
+                            start=4,
+                            end=8,
+                            bits=4,
+                            type="reserved",
+                            description="Unused bits",
+                        ),
+                    ],
+                    name="a_bit_field",
+                    size=1,
+                )
+            }
+        ),
+    ),
+    (
+        "Grouped Structures",
+        {
+            "small_group": {
+                "description": "A Small Example Group",
+                "display_name": "Small Group",
+                "type": "group",
+                "size": 1,
+            },
+            "simple_struct": {
+                "description": "A simple structure",
+                "display_name": "Simple Structure",
+                "type": "structure",
+                "size": 1,
+                "members": [
+                    {
+                        "name": "number",
+                        "size": 1,
+                        "type": "int",
+                        "description": "A small number",
+                    },
+                ],
+                "groups": {
+                    "small_group": {"value": 1, "name": "simple"},
+                },
+            },
+        },
+        TypeDefinitions(
+            {
+                "small_group": Group(
+                    name="small_group",
+                    description="A Small Example Group",
+                    size=1,
+                    members=[
+                        GroupMember(
+                            name="simple", type="simple_struct", value=1
+                        ),
+                    ],
+                ),
+                "simple_struct": Structure(
+                    name="simple_struct",
+                    display_name="Simple Structure",
+                    description="A simple structure",
+                    size=1,
+                    members=[
+                        StructureMember(
+                            name="number",
+                            size=1,
+                            type="int",
+                            description="A small number",
+                        )
+                    ],
+                ),
+            }
+        ),
+    ),
 ]
 
 
@@ -169,3 +363,207 @@ def test_type_definitions(
     debug(result)
     debug(expected)
     assert expected == result
+
+
+def test_example_file() -> None:
+    this_file = Path(__file__).absolute()
+    tests_dir = this_file.parent
+    project_dir = tests_dir.parent
+    example_file = project_dir / "examples/structures.toml"
+    definitions = load_markup_file(example_file)
+
+    result = TypeDefinitions.from_dict(definitions)
+    debug(result)
+
+    expected = TypeDefinitions(
+        {
+            "cmd_reset": Structure(
+                description="Request a software reset",
+                display_name="reset request",
+                members=[],
+                name="cmd_reset",
+                size=0,
+            ),
+            "cmd_temperature_set": Structure(
+                description="Request a change in temperature",
+                display_name="Request temperature change",
+                members=[
+                    StructureMember(
+                        description="Desired temperature",
+                        name="temperature",
+                        size=2,
+                        type="int",
+                    ),
+                    StructureMember(
+                        description="Selected temperature unit",
+                        name="units",
+                        size=1,
+                        type="temperature_units",
+                    ),
+                    StructureMember(
+                        description="reserved",
+                        name="reserved",
+                        size=1,
+                        type="bool",
+                    ),
+                ],
+                name="cmd_temperature_set",
+                size=4,
+            ),
+            "commands": Group(
+                name="commands",
+                description="Debug commands for thermostat",
+                size=2,
+                members=[
+                    GroupMember(
+                        name="reset",
+                        type="cmd_reset",
+                        value=1,
+                    ),
+                    GroupMember(
+                        name="temperature_set",
+                        type="cmd_temperature_set",
+                        value=2,
+                    ),
+                ],
+            ),
+            "hvac_state": BitField(
+                description="State flags for HVAC",
+                display_name="HVAC State",
+                size=1,
+                name="hvac_state",
+                members=[
+                    BitFieldMember(
+                        name="fan_enabled",
+                        start=0,
+                        end=1,
+                        bits=1,
+                        type="bool",
+                        description="Set to true when fan is enabled",
+                    ),
+                    BitFieldMember(
+                        name="reserved_1",
+                        start=1,
+                        end=2,
+                        bits=1,
+                        type="reserved",
+                        description="Reserved",
+                    ),
+                    BitFieldMember(
+                        name="ac_enabled",
+                        start=2,
+                        end=3,
+                        bits=1,
+                        type="bool",
+                        description="Set to true when air conditioning is enabled",
+                    ),
+                    BitFieldMember(
+                        name="heat_enabled",
+                        start=3,
+                        end=4,
+                        bits=1,
+                        type="bool",
+                        description="Set to true when heater is enabled",
+                    ),
+                    BitFieldMember(
+                        name="units",
+                        start=4,
+                        end=5,
+                        bits=1,
+                        type="temperature_units",
+                        description="Units used in thermostat",
+                    ),
+                    BitFieldMember(
+                        name="reserved_5",
+                        start=5,
+                        end=8,
+                        bits=3,
+                        type="reserved",
+                        description="Reserved",
+                    ),
+                ],
+            ),
+            "temperature_units": Enumeration(
+                name="temperature_units",
+                values=[
+                    EnumValue(
+                        label="c",
+                        value=0,
+                        display_name="C",
+                        description="Degrees Celsius",
+                    ),
+                    EnumValue(
+                        label="f",
+                        value=1,
+                        display_name="F",
+                        description="Degrees Fahrenheit",
+                    ),
+                ],
+                description="Units used for temperature",
+                display_name="Temperature Units",
+                size=1,
+            ),
+        }
+    )
+    debug(expected)
+    assert expected == result
+    _expected = TypeDefinitions(
+        {
+            "hvac_state": BitField(
+                description="State flags for HVAC",
+                display_name="HVAC State",
+                size=1,
+                name="hvac_state",
+                members=[
+                    BitFieldMember(
+                        name="fan_enabled",
+                        start=0,
+                        end=1,
+                        bits=1,
+                        type="bool",
+                        description="Set to true when fan is enabled",
+                    ),
+                    BitFieldMember(
+                        name="reserved_1",
+                        start=1,
+                        end=2,
+                        bits=1,
+                        type="reserved",
+                        description="Reserved",
+                    ),
+                    BitFieldMember(
+                        name="ac_enabled",
+                        start=2,
+                        end=3,
+                        bits=1,
+                        type="bool",
+                        description="Set to true when air conditioning is enabled",
+                    ),
+                    BitFieldMember(
+                        name="heat_enabled",
+                        start=3,
+                        end=4,
+                        bits=1,
+                        type="bool",
+                        description="Set to true when heater is enabled",
+                    ),
+                    BitFieldMember(
+                        name="units",
+                        start=4,
+                        end=5,
+                        bits=1,
+                        type="temperature_units",
+                        description="Units used in thermostat",
+                    ),
+                    BitFieldMember(
+                        name="reserved_5",
+                        start=5,
+                        end=8,
+                        bits=3,
+                        type="reserved",
+                        description="Reserved",
+                    ),
+                ],
+            )
+        }
+    )
